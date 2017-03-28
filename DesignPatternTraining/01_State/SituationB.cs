1d6c7f6710a785eec1994dc69bf123046e63fffd
sam <evaoosyz@gmail.com>
2017-03-29 00:39:20 +0800

    情境B 的 狀態模式

---
+++ DesignPatternTraining/01_State/SituationB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTraining._01_State
+{
+    public class SituationB
+    {
+        public Func<int, bool> Submit { get; set; }
+        private bool IsOdd { get; set; }
+        private IFakeService FakeService { get; set; }
+        public SituationB()
+        {
+            IsOdd = true;
+        }
+        public void Work()
+        {
+            var flow = FakeService.GetFlow();
+            //單數才處理
+            if (IsOdd) 
+            {
+                Submit(flow);
+                IsOdd = !IsOdd;
+            }
+        }
+    }
+}
---
+++ DesignPatternTraining/01_State/SituationB_State.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTraining._01_State
+{
+    public class SituationB_State
+    {
+        public Func<int, bool> Submit { get; set; }
+        private IFakeService FakeService { get; set; }
+        private IEventSenderB EventSender { get; set; }
+
+        public SituationB_State(Func<int, bool> submit)
+        {
+            this.Submit = submit;
+            this.EventSender = new OddEventSender(this.Submit);
+        }
+        public void Work()
+        {
+            var flow = FakeService.GetFlow();
+            this.EventSender = this.EventSender.SendEvent(flow);
+        }
+    }
+
+    internal interface IEventSenderB
+    {
+        IEventSenderB SendEvent(int flow);
+    }
+
+    internal class EvenEventSender : IEventSenderB
+    {
+        public Func<int, bool> Trigger { get; set; }
+        public EvenEventSender(Func<int, bool> trigger)
+        {
+            this.Trigger = trigger;
+        }
+        public IEventSenderB SendEvent(int flow)
+        {
+            //偶數不處理
+            return new OddEventSender(this.Trigger);
+        }
+    }
+
+    internal class OddEventSender : IEventSenderB
+    {
+        public Func<int, bool> Trigger { get; set; }
+        public OddEventSender(Func<int, bool> trigger)
+        {
+            this.Trigger = trigger;
+        }
+
+        public IEventSenderB SendEvent(int flow)
+        {
+            this.Trigger(flow);
+            return new EvenEventSender(this.Trigger);
+        }
+    }
+}
