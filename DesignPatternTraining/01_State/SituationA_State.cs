02c909621ab4fffc4410fa6e8a44c5b42b1c9f40
sam <evaoosyz@gmail.com>
2017-03-29 00:03:48 +0800

    完成 狀態模式 完成 輕量版狀態模式

---
+++ DesignPatternTraining/01_State/SituationA_State.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTraining._01_State
+{
+    public class SituationA_State
+    {
+        public Func<int, bool> Submit { get; set; }
+        private bool IsFirst { get; set; }
+        private IFakeService FakeService { get; set; }
+        private IEventSender EventSender { get; set; }
+
+        public SituationA_State(Func<int, bool> submit)
+        {
+            this.Submit = submit;
+            this.EventSender = new NotExecuteEventSender(this.Submit);
+        }
+        public void Work()
+        {
+            var flow = FakeService.GetFlow();
+            this.EventSender.SendEvent(flow);
+            this.EventSender = this.EventSender.Open();
+        }
+    }
+
+    internal interface IEventSender
+    {
+        bool SendEvent(int flow);
+        IEventSender Open();
+    }
+
+    internal class NotExecuteEventSender : IEventSender
+    {
+        public Func<int, bool> Trigger { get; set; }
+        public NotExecuteEventSender(Func<int, bool> trigger)
+        {
+            this.Trigger = trigger;
+        }
+        public IEventSender Open()
+        {
+            return new RegularEventSender(this.Trigger);
+        }
+
+        public bool SendEvent(int flow)
+        {
+            //第一次不處理
+            return false;
+        }
+    }
+
+    internal class RegularEventSender : IEventSender
+    {
+        public Func<int, bool> Trigger { get; set; }
+        public RegularEventSender(Func<int, bool> trigger)
+        {
+            this.Trigger = trigger;
+        }
+        public IEventSender Open()
+        {
+            return this;
+        }
+
+        public bool SendEvent(int flow)
+        {
+            return this.Trigger(flow);
+        }
+    }
+}
---
+++ DesignPatternTraining/01_State/SituationA_StateLite.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTraining._01_State
+{
+    public class SituationA_StateLite
+    {
+        public Func<int, bool> Submit { get; set; }
+        private bool IsFirst { get; set; }
+        private IFakeService FakeService { get; set; }
+        private EventSender EventSender { get; set; }
+
+        public SituationA_StateLite(Func<int, bool> submit)
+        {
+            this.Submit = submit;
+            this.EventSender = new EventSender(x => false);
+        }
+        public void Work()
+        {
+            var flow = FakeService.GetFlow();
+            this.EventSender.SendEvent(flow);
+            this.EventSender.Open(this.Submit);
+        }
+    }
+
+    
+    internal class EventSender
+    {
+        public Func<int, bool> Trigger { get; set; }
+        public EventSender(Func<int, bool> trigger)
+        {
+            //建構子進來的trigger是不做事的
+            this.Trigger = trigger;
+        }
+        public void Open(Func<int, bool> trigger)
+        {
+            //Open 才會傳真正的事件
+            this.Trigger = trigger;
+        }
+
+        public bool SendEvent(int flow)
+        {
+            return this.Trigger(flow);
+        }
+    }
+
+    
+}
